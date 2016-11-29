832f06a43081bcdc62a0ac15688149d6315533a3
Phuong Nguyen <pnguye11@gmail.com>
2016-11-29 14:22:51 -0800

    home and about pages

--- app/app.component.js
+++ app/app.component.js
@@ -11,14 +11,16 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var navbar_component_1 = require('./components/navbar/navbar.component');
 var jumbotron_component_1 = require('./components/jumbotron/jumbotron.component');
+var home_component_1 = require('./components/pages/home.component');
+var about_component_1 = require('./components/pages/about.component');
 var AppComponent = (function () {
     function AppComponent() {
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<navbar></navbar><jumbotron></jumbotron>',
-            directives: [navbar_component_1.NavbarComponent, jumbotron_component_1.JumbotronComponent]
+            template: "<navbar></navbar>\n             <jumbotron></jumbotron>\n             <div class=\"container\">\n             <home></home>\n             <about></about>\n             </div>\n             ",
+            directives: [navbar_component_1.NavbarComponent, jumbotron_component_1.JumbotronComponent, home_component_1.HomeComponent, about_component_1.AboutComponent]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,19 @@
 import {Component} from '@angular/core';
 import {NavbarComponent} from './components/navbar/navbar.component';
 import {JumbotronComponent} from './components/jumbotron/jumbotron.component';
+import {HomeComponent} from './components/pages/home.component';
+import {AboutComponent} from './components/pages/about.component';
 
 @Component({
   selector: 'my-app',
-  template: '<navbar></navbar><jumbotron></jumbotron>',
-  directives: [NavbarComponent, JumbotronComponent]
+  template: `<navbar></navbar>
+             <jumbotron></jumbotron>
+             <div class="container">
+             <home></home>
+             <about></about>
+             </div>
+             `,
+  directives: [NavbarComponent, JumbotronComponent, HomeComponent, AboutComponent]
 })
 
 export class AppComponent{};
